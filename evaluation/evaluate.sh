d5f5bd0e316b2536b6c4d5c1c874a72496885bad
Uwe Sommerlatt <uwe.sommerlatt@googlemail.com>
2012-06-15 19:10:06 +0200

    Added quality function test and corresponding test data.

--- evaluation/evaluate.sh
+++ evaluation/evaluate.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 TESTNAME=$1
-PROBLEM=$2
-G=$3
+NUMBEROFRUNS=$2
+PROBLEM=$3
+G=$4
 
 S=500
 Q=1
@@ -12,15 +13,13 @@ M='shift'
 D='best-fit'
 ES="tournament=3"
 
-NUMBEROFRUNS=11
 
 function execute {
   NUMBER=$(printf "%02d" $1)
   PREFIX='test_'
   EXTENSION='.dat'
   DATAFOLDER="evaluation/data/"
-  O="${DATAFOLDER}${PREFIX}SCRIPTTEST_${ALGORITHM}_${PROBLEM}_${NUMBER}${EXTENSION}"
-  #O="${DATAFOLDER}${PREFIX}${TESTNAME}_${ALGORITHM}_${PROBLEM}_${NUMBER}${EXTENSION}"
+  O="${DATAFOLDER}${PREFIX}${TESTNAME}_${ALGORITHM}_${PROBLEM}_${NUMBER}${EXTENSION}"
   sbt "run -s $S -g $G -d $D -q $Q -ps $PS -r $R -m $M -es $ES -o $O $PROBLEM"
 }
 
@@ -34,6 +33,18 @@ if [ "$TESTNAME" == "decfuc" ]; then
        for ALGORITHM in ${ALGORITHMS[@]}
        do
          D=$ALGORITHM
+         execute $TESTRUN
+       done
+    done
+fi
+
+if [ "$TESTNAME" == "qualfunc" ]; then
+    declare -a ALGORITHMS=('q1' 'q2')
+    for TESTRUN in $(eval echo {1..$NUMBEROFRUNS})
+    do
+       for ALGORITHM in ${ALGORITHMS[@]}
+       do
+         Q=${ALGORITHM:1}
          execute $TESTRUN
        done
     done
