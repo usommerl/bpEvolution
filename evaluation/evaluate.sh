08b5853fd52a05fb739af024e1bff413f5c3f2a7
Uwe Sommerlatt <uwe.sommerlatt@googlemail.com>
2012-06-16 18:33:30 +0200

    Added another test to the evaluation script and its corresponding test data.

--- evaluation/evaluate.sh
+++ evaluation/evaluate.sh
@@ -15,61 +15,83 @@ ES="tournament=3"
 
 
 function execute {
-  NUMBER=$(printf "%02d" $1)
-  PREFIX='test_'
-  EXTENSION='.dat'
-  DATAFOLDER="evaluation/data/"
-  O="${DATAFOLDER}${PREFIX}${TESTNAME}_${ALGORITHM}_${PROBLEM}_${NUMBER}${EXTENSION}"
-  sbt "run -s $S -g $G -d $D -q $Q -ps $PS -r $R -m $M -es $ES -o $O $PROBLEM"
+   PREFIX='test'
+   EXTENSION='.dat'
+   DATAFOLDER="evaluation/data/"
+   if [ $# -eq 1 ];then
+       NUMBER=$(printf "%02d" $1)
+       O="${DATAFOLDER}${PREFIX}_${TESTNAME}_${ALGORITHM}_${PROBLEM}_${NUMBER}${EXTENSION}"
+   else
+       O="${DATAFOLDER}${PREFIX}_${TESTNAME}_${PROBLEM}${EXTENSION}"
+   fi
+   sbt "run -s $S -g $G -d $D -q $Q -ps $PS -r $R -m $M -es $ES -o $O $PROBLEM"
 }
 
 BUILDFOLDER="../"
 cd $BUILDFOLDER
 
-if [ "$TESTNAME" == "decfuc" ]; then
-    declare -a ALGORITHMS=('next-fit' 'first-fit' 'best-fit')
-    for TESTRUN in $(eval echo {1..$NUMBEROFRUNS})
-    do
-       for ALGORITHM in ${ALGORITHMS[@]}
-       do
-         D=$ALGORITHM
-         execute $TESTRUN
-       done
-    done
-fi
+if [ $# -gt 2 ]; then
 
-if [ "$TESTNAME" == "qualfunc" ]; then
-    declare -a ALGORITHMS=('q1' 'q2')
-    for TESTRUN in $(eval echo {1..$NUMBEROFRUNS})
-    do
-       for ALGORITHM in ${ALGORITHMS[@]}
-       do
-         Q=${ALGORITHM:1}
-         execute $TESTRUN
-       done
-    done
-fi
+    if [ "$TESTNAME" == "decfuc" ]; then
+        declare -a ALGORITHMS=('next-fit' 'first-fit' 'best-fit')
+        for TESTRUN in $(eval echo {1..$NUMBEROFRUNS})
+        do
+           for ALGORITHM in ${ALGORITHMS[@]}
+           do
+             D=$ALGORITHM
+             execute $TESTRUN
+           done
+        done
+    fi
+    if [ "$TESTNAME" == "qualfunc" ]; then
+        declare -a ALGORITHMS=('q1' 'q2')
+        for TESTRUN in $(eval echo {1..$NUMBEROFRUNS})
+        do
+           for ALGORITHM in ${ALGORITHMS[@]}
+           do
+             Q=${ALGORITHM:1}
+             execute $TESTRUN
+           done
+        done
+    fi
+    if [ "$TESTNAME" == "mutations" ]; then
+        declare -a ALGORITHMS=('none' 'inversion' 'shift' 'exchange' 'shift,exchange')
+        for TESTRUN in $(eval echo {1..$NUMBEROFRUNS})
+        do
+           for ALGORITHM in ${ALGORITHMS[@]}
+           do
+             M=$ALGORITHM
+             execute $TESTRUN
+           done
+        done
+    fi
+    if [ "$TESTNAME" == "recombinations" ]; then
+        declare -a ALGORITHMS=('ordered' 'mapped' 'random')
+        for TESTRUN in $(eval echo {1..$NUMBEROFRUNS})
+        do
+           for ALGORITHM in ${ALGORITHMS[@]}
+           do
+             R=$ALGORITHM
+             execute $TESTRUN
+           done
+        done
+    fi
 
-if [ "$TESTNAME" == "mutations" ]; then
-    declare -a ALGORITHMS=('none' 'inversion' 'shift' 'exchange' 'shift,exchange')
-    for TESTRUN in $(eval echo {1..$NUMBEROFRUNS})
-    do
-       for ALGORITHM in ${ALGORITHMS[@]}
-       do
-         M=$ALGORITHM
-         execute $TESTRUN
-       done
-    done
-fi
+else
+    
+    R='random'
+    G=$2
+    if [ "$TESTNAME" == "comparison" ]; then
+       # declare -a PROBLEMPREFIXES=('u120_' 't60_')
+        declare -a PROBLEMPREFIXES=('t60_')
+        for PROBLEMPREFIX in ${PROBLEMPREFIXES[@]}
+        do
+        for PROBLEMNUMBER in {0..19}
+           do
+             PROBLEM=${PROBLEMPREFIX}$(printf "%02d" $PROBLEMNUMBER)
+             execute
+           done
+        done
+    fi
 
-if [ "$TESTNAME" == "recombinations" ]; then
-    declare -a ALGORITHMS=('ordered' 'mapped' 'random')
-    for TESTRUN in $(eval echo {1..$NUMBEROFRUNS})
-    do
-       for ALGORITHM in ${ALGORITHMS[@]}
-       do
-         R=$ALGORITHM
-         execute $TESTRUN
-       done
-    done
 fi
