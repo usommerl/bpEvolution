9d454a671fc547fa5a8ee286489ebee5cbcf99cf
Uwe Sommerlatt <uwe.sommerlatt@googlemail.com>
2012-06-19 15:01:06 +0200

    Fixed double precision bug in calculation of theoretical optimum.

--- src/main/scala/problem/BinPackProblem.scala
+++ src/main/scala/problem/BinPackProblem.scala
@@ -5,11 +5,12 @@ class BinPackProblem(val id: String, val binCapacity: Double, val bestKnownSolut
     require(id.length > 0)
     require(items.size > 0)
     require(bestKnownSolution > 0)
-    
+
     lazy val size = this.items.size
-    lazy val theoreticalOptimum = ceil((0.0 /: items)(_+_.size) / this.binCapacity).toInt
+    private lazy val sumItemSizes = (BigDecimal("0.0") /: items)( (s,i) => s + BigDecimal(i.size.toString))
+    lazy val theoreticalOptimum = ceil(sumItemSizes.toDouble/binCapacity).toInt
 
     override def toString() = {
-      "[BinPackProblem id="+id+" binCapacity="+binCapacity+" bestKnownSolution="+bestKnownSolution+"]" 
+      "[BinPackProblem id="+id+" binCapacity="+binCapacity+" theoreticalOptimum="+theoreticalOptimum+" bestKnownSolution="+bestKnownSolution+"]" 
     }
 } 
